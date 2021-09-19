43989ce6e90f7e7217b76568f17c2da55d853fc5
masayoshi yamasaki <lolemsakkaku@gmail.com>
2021-09-19 10:22:12 +0900

    mix bootstrap.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.react('resources/js/index.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+    .react('resources/js/bootstrap.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
 
 if(mix.inProduction()){
     mix.version()
