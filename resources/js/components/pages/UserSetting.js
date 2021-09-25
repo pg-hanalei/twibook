94b983f63b7b7bd0986b693146600a7f583d216f
masayoshi yamasaki <lolemsakkaku@gmail.com>
2021-09-25 19:31:45 +0900

    before react-easy-crop

--- resources/js/components/pages/UserSetting.js
+++ resources/js/components/pages/UserSetting.js
@@ -3,6 +3,15 @@ import MenuSelect from "../molecules/MenuSelect";
 import PageTitle from "../atoms/PageTitle";
 
 const UserSetting = () => {
+
+    const onChangeImage = () => {
+        document.getElementById('modalButton').click();
+    }
+
+    const onClickSave = () => {
+        console.log(document.getElementById("button_save"));
+    }
+
     return (
         <>
             <div className="container">
@@ -11,13 +20,15 @@ const UserSetting = () => {
                 <div className="mt-4">
                     <form>
                         <div className="form-group">
-                            <label htmlFor="user_image" id="user_image-area">
+                            <label htmlFor="user_image_input" id="user_image-area">
                                 <img id="user_image" src="https://source.unsplash.com/bIhpiQA009k" alt="プロフィール画像" className="rounded-circle"
                                      width="180px" height="180px"/>
-                                <i className="bi bi-camera-fill user_image--icon"></i>
+                                <i className="bi bi-camera-fill user_image_icon"></i>
                             </label>
                             <input type="file" className="form-control d-none"
-                                   aria-describedby="user_image" placeholder="user_image"/>
+                                   aria-describedby="user_image" placeholder="user_image"
+                                   onChange={onChangeImage} id="user_image_input"
+                            />
                         </div>
                         <div className="form-group">
                             <label htmlFor="exampleInputEmail1">Email address</label>
@@ -37,6 +48,30 @@ const UserSetting = () => {
                         </div>
                         <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
+                </div>
+
+                {/*画像切り取りモーダル*/}
+                <button style={{display:"none"}} type="button" id="modalButton" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                    Launch demo modal
+                </button>
+                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            {/*<div className="modal-header">*/}
+                            {/*    <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>*/}
+                            {/*    <button type="button" className="close" data-dismiss="modal" aria-label="Close">*/}
+                            {/*        <span aria-hidden="true">&times;</span>*/}
+                            {/*    </button>*/}
+                            {/*</div>*/}
+                            <div className="modal-body">
+                                ...
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button id="button_save" type="button" className="btn btn-primary" data-dismiss="modal" onClick={onClickSave}>Save changes</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
